b5e7cad64a9c36335f4df783f244bef9e56ad302
Pawel Zajglicz <pawel.zajglicz@gmail.com>
2020-09-29 20:54:49 +0200

    adding stored procedures

--- DatabaseProject/AspNetCoreCommon/TimsDinerDB/Scripts/Script.PostDeployment.sql
+++ DatabaseProject/AspNetCoreCommon/TimsDinerDB/Scripts/Script.PostDeployment.sql
@@ -9,3 +9,11 @@ Post-Deployment Script Template
                SELECT * FROM [$(TableName)]					
 --------------------------------------------------------------------------------------
 */
+
+if not exists (select * from dbo.Food)
+begin 
+    insert into dbo.Food(Title, [Description], Price)
+    values ('Cheeseburger Meal', 'A cheeseburger, fries and a drink', 5.95),
+    ('Chili Dog Meal', 'Two chili dogs, fries and a drink', 4.15),
+    ('Vegan Meal', 'A large salad and a water', 1.95);
+end
