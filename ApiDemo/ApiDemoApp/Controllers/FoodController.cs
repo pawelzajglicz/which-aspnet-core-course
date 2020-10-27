7862838a352b71ea0d07790cc46ff3a3583a8372
Pawel Zajglicz <pawel.zajglicz@gmail.com>
2020-10-27 22:10:20 +0100

    reading data in api project

---
+++ ApiDemo/ApiDemoApp/Controllers/FoodController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DataLibrary.Data;
+using DataLibrary.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiDemoApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FoodController : ControllerBase
+    {
+        private readonly IFoodData foodData;
+
+        public FoodController(IFoodData foodData)
+        {
+            this.foodData = foodData;
+        }
+
+        [HttpGet]
+        public async Task<List<FoodModel>> Get()
+        {
+            return await foodData.GetFood();
+        }
+    }
+}
